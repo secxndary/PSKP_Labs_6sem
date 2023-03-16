63dfeff1c79f80f67b060e77db9695c5b8af7d39
secxndary <valdaitsevv@mail.ru>
2023-03-16 16:33:37 +0300

    Lab 18: add delete services

--- PSKP_Lab18/18-01.js
+++ PSKP_Lab18/18-01.js
@@ -1,18 +1,12 @@
 import { Sequelize } from 'sequelize';
 import express from 'express';
-import {
-    faculty,
-    pulpit,
-    subject,
-    teacher,
-    auditorium_type,
-    auditorium
-} from './models/models.js';
+import SequelizeService from './services.js';
+import { faculty, pulpit, subject, teacher, auditorium_type, auditorium } from './models/models.js';
 
-
-const app = express();
-const prefix = '/api';
 const __dirname = 'C:\\Users\\valda\\source\\repos\\semester#6\\ПСКП\\PSKP_Lab18\\static\\';
+const prefix = '/api';
+const app = express();
+const service = new SequelizeService;
 const sequelize = new Sequelize('sequel', 'postgres', '1111', {
     host: 'localhost',
     dialect: 'postgres',
@@ -22,24 +16,22 @@ const sequelize = new Sequelize('sequel', 'postgres', '1111', {
 
 
 
-app.get(prefix + '/user', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
-})
-    .get(prefix + '/faculties', (req, res) => {
-        faculty.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
-    })
-    .get(prefix + '/pulpits', (req, res) => {
-        pulpit.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
-    })
-    .get(prefix + '/subjects', (req, res) => {
-        subject.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
-    })
-    .get(prefix + '/auditoriumstypes', (req, res) => {
-        auditorium_type.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
-    })
-    .get(prefix + '/auditoriums', (req, res) => {
-        auditorium.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
-    })
+app.get('/', (req, res) => { res.sendFile(__dirname + 'index.html'); });
+
+app.get(prefix + '/faculties', (req, res) => { service.getFaculties(res); })
+    .get(prefix + '/pulpits', (req, res) => { service.getPulpits(res); })
+    .get(prefix + '/teachers', (req, res) => { service.getTeachers(res); })
+    .get(prefix + '/subjects', (req, res) => { service.getSubjects(res); })
+    .get(prefix + '/auditoriumstypes', (req, res) => { service.getAuditoriumTypes(res); })
+    .get(prefix + '/auditoriums', (req, res) => { service.getAuditoriums(res); });
+
+app.delete(prefix + '/faculties/:faculty', (req, res) => { service.deleteFaculty(res, req.params['faculty']); })
+    .delete(prefix + '/pulpits/:pulpit', (req, res) => { service.deletePulpit(res, req.params['pulpit']); })
+    .delete(prefix + '/subjects/:subject', (req, res) => { service.deleteSubject(res, req.params['subject']); })
+    .delete(prefix + '/auditoriumtypes/:type', (req, res) => { service.deleteAuditoriumType(res, req.params['type']); })
+    .delete(prefix + '/auditoriums/:auditorium', (req, res) => { service.deleteAuditorium(res, req.params['auditorium']); })
+
+
 
 
 
---
+++ PSKP_Lab18/services.js
@@ -0,0 +1,99 @@
+import { faculty, pulpit, subject, teacher, auditorium_type, auditorium } from './models/models.js';
+import { Sequelize } from 'sequelize';
+const sequelize = new Sequelize('sequel', 'postgres', '1111', {
+    host: 'localhost',
+    dialect: 'postgres',
+    define: { timestamps: false }
+});
+
+
+
+
+export default class SequelizeService {
+
+
+    // =====================================  GET  =====================================
+
+    getFaculties = async res => {
+        await faculty.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    }
+
+    getPulpits = async res => {
+        await pulpit.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    }
+
+    getSubjects = async res => {
+        await subject.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    }
+
+    getTeachers = async res => {
+        await teacher.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    }
+
+    getAuditoriumTypes = async res => {
+        await auditorium_type.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    }
+
+    getAuditoriums = async res => {
+        await auditorium.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    }
+
+
+
+    // =====================================  DELETE  =====================================
+
+    deleteFaculty = async (res, faculty_id) => {
+        const facultyToDelete = await faculty.findByPk(faculty_id);
+        await faculty.destroy({ where: { faculty: faculty_id } })
+            .then(() => {
+                if (!facultyToDelete)
+                    res.send('иди нахуй');  // todo: заменить на сообщение об ошибке
+                else
+                    res.send(JSON.stringify(facultyToDelete, null, 4));
+            });
+    }
+
+    deletePulpit = async (res, pulpit_id) => {
+        const pulpitToDelete = await pulpit.findByPk(pulpit_id);
+        await pulpit.destroy({ where: { pulpit: pulpit_id } })
+            .then(() => {
+                if (!pulpitToDelete)
+                    res.send('иди нахуй');  // todo: заменить на сообщение об ошибке
+                else
+                    res.send(JSON.stringify(pulpitToDelete, null, 4));
+            });
+    }
+
+    deleteSubject = async (res, subject_id) => {
+        const subjectToDelete = await subject.findByPk(subject_id);
+        await subject.destroy({ where: { subject: subject_id } })
+            .then(() => {
+                if (!subjectToDelete)
+                    res.send('иди нахуй');  // todo: заменить на сообщение об ошибке
+                else
+                    res.send(JSON.stringify(subjectToDelete, null, 4));
+            });
+    }
+
+    deleteAuditoriumType = async (res, type_id) => {
+        const typeToDelete = await auditorium_type.findByPk(type_id);
+        await auditorium_type.destroy({ where: { auditorium_type: type_id } })
+            .then(() => {
+                if (!typeToDelete)
+                    res.send('иди нахуй');  // todo: заменить на сообщение об ошибке
+                else
+                    res.send(JSON.stringify(typeToDelete, null, 4));
+            });
+    }
+
+    deleteAuditorium = async (res, auditorium_id) => {
+        const auditoriumToDelete = await auditorium.findByPk(auditorium_id);
+        await auditorium.destroy({ where: { auditorium: auditorium_id } })
+            .then(() => {
+                if (!auditoriumToDelete)
+                    res.send('иди нахуй');  // todo: заменить на сообщение об ошибке
+                else
+                    res.send(JSON.stringify(auditoriumToDelete, null, 4));
+            });
+    }
+}
