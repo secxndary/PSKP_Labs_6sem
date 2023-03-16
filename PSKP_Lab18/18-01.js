be57d5d290a8d5b071fe1aac83c0e41f7af07a45
secxndary <valdaitsevv@mail.ru>
2023-03-16 10:52:58 +0300

    feat: add get hanlers (lab 18)

--- PSKP_Lab18/18-01.js
+++ PSKP_Lab18/18-01.js
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize';
+import express from 'express';
 import {
     faculty,
     pulpit,
@@ -7,6 +8,11 @@ import {
     auditorium_type,
     auditorium
 } from './models/models.js';
+
+
+const app = express();
+const prefix = '/api';
+const __dirname = 'C:\\Users\\valda\\source\\repos\\semester#6\\ПСКП\\PSKP_Lab18\\static\\';
 const sequelize = new Sequelize('sequel', 'postgres', '1111', {
     host: 'localhost',
     dialect: 'postgres',
@@ -15,32 +21,43 @@ const sequelize = new Sequelize('sequel', 'postgres', '1111', {
 
 
 
+
+app.get(prefix + '/user', (req, res) => {
+    res.sendFile(__dirname + 'index.html');
+})
+    .get(prefix + '/faculties', (req, res) => {
+        faculty.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    })
+    .get(prefix + '/pulpits', (req, res) => {
+        pulpit.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    })
+    .get(prefix + '/subjects', (req, res) => {
+        subject.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    })
+    .get(prefix + '/auditoriumstypes', (req, res) => {
+        auditorium_type.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    })
+    .get(prefix + '/auditoriums', (req, res) => {
+        auditorium.findAll().then(items => { res.send(JSON.stringify(items, null, 4)); });
+    })
+
+
+
+
+
 sequelize.authenticate()
     .then(() => {
         console.log('[OK] Connected to database.\n');
-
-        faculty.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
-        pulpit.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
-        teacher.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
-        subject.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
-        auditorium_type.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
-        auditorium.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
-
-
+        // faculty.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
+        // pulpit.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
+        // teacher.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
+        // subject.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
+        // auditorium_type.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
+        // auditorium.findAll().then(items => { console.log(JSON.stringify(items, null, 4)); });
         // sequelize.close();
     })
-    .catch(err => {
-        console.log('[ERROR] Sequelize: ', err);
-    })
+    .catch(err => { console.log('[ERROR] Sequelize: ', err); });
 
 
 
-
-
-
-// let http_handler = (req, res) => {
-// }
-// server.listen(5000, () => { console.log('\n[INFO] Server running at localhost:5000/\n'); })
-//     .on('error', error => { console.log('\n[ERROR] ', error.message); })
-//     .on('request', http_handler);
-
+app.listen(5000);
