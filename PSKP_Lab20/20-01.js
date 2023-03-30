f481dbf7f9289f6d91272fe5d4deec27e8923d07
secxndary <valdaitsevv@mail.ru>
2023-03-30 17:38:51 +0300

    да ну его нахуй

--- PSKP_Lab20/20-01.js
+++ PSKP_Lab20/20-01.js
@@ -1,38 +1,16 @@
-const handlebar = require('express-handlebars');
-const bodyParser = require('body-parser');
-const express = require('express');
-// const authorRouter = require('./routes/authorRouter.js');
-const { Sequelize } = require('sequelize');
-
+import handlebar from 'express-handlebars';
+import bodyParser from 'body-parser';
+import express from 'express';
+import authorRouter from './routes/authorRouter.js';
 
 const port = process.env.PORT ? process.env.PORT : 3000;
 const hbs = handlebar.create({ extname: '.hbs' });
 const app = express();
-const sequelize = new Sequelize('Sublish', 'sa', '1111', {
-    host: 'MSSQLLocalDB\\(localDB)',
-    dialect: 'mssql',
-    dialectModule: require('msnodesqlv8/lib/sequelize'),
-    bindParam: false,
-    /*logging: false,*/
-    dialectOptions: {
-        options: {
-            connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=(LocalDB)\\MSSQLLocalDB;Database=Sublish;Trusted_Connection=yes;',
-        },
-    },
-    define: {
-        timestamps: false,
-    }
-});
-
-sequelize.authenticate()
-    .then(() => { console.log('[OK] Connected to database.\n'); })
-    .catch(err => {
-        console.log('[ERROR] Sequelize: ', err);
-        sequelize.close();
-    });
 
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/author', authorRouter);
 
 
 
@@ -40,7 +18,5 @@ app.get('/', (req, res) => { res.render('index', { layout: null, hw: 'hello worl
 
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
-// app.use('/author', authorRouter);
 
 app.listen(port, () => console.log(`[OK] Server running at localhost:${port}/\n`));
