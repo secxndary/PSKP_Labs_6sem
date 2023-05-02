318b512b3291a631cf3e20012e32cd721b3e6e69
secxndary <valdaitsevv@mail.ru>
2023-05-02 22:09:01 +0300

    Lab 22 carcass

--- PSKP_Lab22/22-01.js
+++ PSKP_Lab22/22-01.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const port = 5000;
 
-app.use(express.static(__dirname + '/public'));
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+const session = require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'qweqwe'
+});
+
+const port = 5000;
+const users = fs.readFileSync('users.json');
+
+
+app.use(express.static(__dirname + '/static'));
 app.use(express.json());
+app.use(session);
+app.use(passport.initialize());
+passport.use(new BasicStrategy((user, password, done) => {
+    console.log('passport.use ', user, password);
+    let rc = null;
+    let credentials = getCredentials(user);
+    if (!credentials)
+        rc = done(null, false, { message: 'incorrect username' });
+    else if (!verifyPassword(credentials.password, password))
+        rc = done(null, false, { message: 'incorrect password' });
+    else
+        rc = done(null, user);
+    return rc;
+}));
+
+
 
 
 
@@ -13,14 +41,22 @@ app.get('/login', (req, res) => {
 
 app.get('logout', (req, res) => {
     // Отключить аутентифицированный доступ к ресурсу 
+    req.session.logout = true;
+    delete req.headers['authorization'];
 });
 
 app.get('/resource', (req, res) => {
     // Ресурс отправляет сообщение RESOURCE
+    res.send('RESOURCE');
     // При попытке неаутентифицированного доступа выполняет переадресацию на GET /login 
 });
 
 // other URIs => 404 error
 
+
+
+
+const getCredentials = user => users.find(e => e.user.toUpperCase() == user.toUpperCase());
+const verifyPassword = (firstPassword, secondPassword) => firstPassword == secondPassword;
 
 app.listen(process.env.PORT || port, () => console.log(`[OK] Server running at localhost:${port}/\n`));
