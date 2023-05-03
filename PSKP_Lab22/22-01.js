69dc9e9fde8a711b55ce118b6db405420f008861
secxndary <valdaitsevv@mail.ru>
2023-05-03 14:23:35 +0300

    impl basic-auth, refactoring using passport-http

--- PSKP_Lab22/22-01.js
+++ PSKP_Lab22/22-01.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const app = express();
 
 const passport = require('passport');
+const auth = require('basic-auth');
 const BasicStrategy = require('passport-http').BasicStrategy;
 const session = require('express-session')({
     resave: false,
@@ -13,10 +14,11 @@ const session = require('express-session')({
 const port = 5000;
 const users = fs.readFileSync('users.json');
 
-
 app.use(express.static(__dirname + '/static'));
 app.use(express.json());
 app.use(session);
+
+
 app.use(passport.initialize());
 passport.use(new BasicStrategy((user, password, done) => {
     console.log('passport.use ', user, password);
@@ -35,17 +37,24 @@ passport.use(new BasicStrategy((user, password, done) => {
 
 
 
-app.get('/login', (req, res) => {
-    // Ввод имени и пароля для аутентифицированного доступа к ресурсу
-});
+// Ввод имени и пароля для аутентифицированного доступа к ресурсу
+app.get(
+    '/login',
+    passport.authenticate('basic', { session: false }),
+    (req, res, next) => { res.redirect('/resource'); });
 
-app.get('logout', (req, res) => {
+
+
+
+
+
+app.get('/logout', (req, res, next) => {
     // Отключить аутентифицированный доступ к ресурсу 
     req.session.logout = true;
     delete req.headers['authorization'];
 });
 
-app.get('/resource', (req, res) => {
+app.get('/resource', (req, res, next) => {
     // Ресурс отправляет сообщение RESOURCE
     res.send('RESOURCE');
     // При попытке неаутентифицированного доступа выполняет переадресацию на GET /login 
@@ -55,8 +64,28 @@ app.get('/resource', (req, res) => {
 
 
 
-
-const getCredentials = user => users.find(e => e.user.toUpperCase() == user.toUpperCase());
+const check = (creds1, creds2) => creds1.name.toUpperCase() == creds2.name.toUpperCase() && creds1.pass == creds2.pass;
+// const getCredentials = name => users.find(u => u.name.toUpperCase() == name.toUpperCase());
 const verifyPassword = (firstPassword, secondPassword) => firstPassword == secondPassword;
 
 app.listen(process.env.PORT || port, () => console.log(`[OK] Server running at localhost:${port}/\n`));
+
+
+
+
+
+
+    // const mycred = { name: "secxndary", pass: "qwe" };
+    // const credentials = auth(req);
+    // if (!credentials) {
+    //     res.status(401).append('WWW-Authenticate', 'Basic realm="Resurse-realm"').send('Access denied');
+    //     console.log('denied: ', credentials);
+    // }
+    // else if (!check(credentials, mycred)) {
+    //     res.status(401).append('WWW-Authenticate', 'Basic realm="Resurse-realm"').send('Incorrect name or pass');
+    //     console.log('incorrect: ', credentials);
+    // }
+    // else {
+    //     res.redirect('/resource');
+    //     console.log('redirect resource: ', credentials);
+    // }
